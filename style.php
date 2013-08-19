981e5f69d6548ff027230dfe52eeb404ce1daa95
Zaher Dirkey <zaherdirkey@yahoo.com>
2013-08-20 00:46:19 +0300

    Working on replace with Changer functions

--- style.php
+++ style.php
@@ -1,14 +1,19 @@
 <?php
 header("Content-type: text/css; charset: UTF-8");
 header('Content-type: text/css');
-header('Cache-control: must-revalidate');
+//header('Cache-control: must-revalidate');
 
 include('inc/changer.php');
-
+/**
+  using get make it more faster, we will call get_theme_mod('color_scheme')
+  in the header of theme, no need to use SQL/Classes of wordpress here.
+*/
 $css_changer = new changer();
-$name = 'gray'; //get_theme_mod('color_scheme');
-$css_changer->load_values_ini(dirname(__FILE__).'\\'.$name.'.style.ini');
-$style = file_get_contents(dirname(__FILE__).'\\style.css');
-echo $css_changer->generate($style);
+if (isset($_GET['style']) and !empty($_GET['style']))
+  $name = $_GET['style'];
+else
+  $name = 'gray'; //default;
+
+changer_print_css_file(dirname(__FILE__).'\\style.css', dirname(__FILE__).'\\'.$name.'.style.ini');
 
 ?>
