131408b10febdbcd834d84260575b74dc9531351
Zaher Dirkey <zaherdirkey@yahoo.com>
2014-03-08 21:18:24 +0200

    trying to split main layout in a separated css file

--- header.php
+++ header.php
@@ -52,6 +52,7 @@
   <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
   <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
   <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
+  <link rel='stylesheet' href="<?php print get_stylesheet_directory_uri(); ?>/layout.css" type='text/css' />
   <?php if (empty($pass)) { ?>
   <link rel='stylesheet' href="<?php print get_stylesheet_directory_uri(); ?>/css/style.css" type='text/css' />
   <?php } else { ?>
