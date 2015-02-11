fd2917345ba2004837f8640f60c1c32e141be9f2
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-02-11 08:51:41 +0200

    fix bottom foot-bar increase font size

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
   <?php
     get_template_part('post', 'inc');
   ?>
+
   <ul class="pagination">
     <li class="previous-posts"><?php previous_posts_link('&laquo; Previous', 'default') ?></li>
     <li class="next-posts"><?php next_posts_link(__('Next &raquo;', 'default')) ?></li>
