65f1bcbb8ff346a54a64b8e66eae4f5410d56453
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-03-01 09:54:03 +0200

    moved macro to php-macro repo

--- inc/get.sh
+++ inc/get.sh
@@ -1,8 +1,15 @@
 ##
+#   https://github.com/parmaja/php-macro
+#   Author: Zaher Dirkey <http://dirkey.com>
+#
+
+curl --output color.php "https://raw.githubusercontent.com/parmaja/php-macro/master/src/macro.php"
+
+##
 #   https://github.com/mexitek/phpColors
 #   Author: Arlo Carreon <http://arlocarreon.com>
 #
 
-curl --output color.php "https://raw.githubusercontent.com/mexitek/phpColors/master/src/Mexitek/PHPColors/Color.php"
+curl --output color.php "https://raw.githubusercontent.com/parmaja/php-macro/master/src/color.php"
 
 # I dislike using submodules
